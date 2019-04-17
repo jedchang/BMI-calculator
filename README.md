2fc8f91340693de0db861f3e403493e46128f2ae
Jed Chang <march.24@gmail.com>
2019-04-17 16:25:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,58 @@
+# BMI Calculator
+
+BMI 計算器
+
+![image](https://img.shields.io/badge/JavaScript-exercise-brightgreen.svg)
+![image](https://img.shields.io/badge/SASS-exercise-ff69b4.svg)
+
+![images](https://github.com/jedchang/BMI-Calculator/blob/master/preview.jpg)
+
+## 資料
+
+- `JSON.parse()`：將字串轉成物件。
+- `JSON.stringify()`：將陣列轉字串。
+- `localStorage.getItem()`：將值取出 localStorage。
+- `localStorage.setItem()`：將值存入 localStorage。
+
+## BMI 計算
+
+- BMI 計算 = 體重(公斤)除以身高(公尺)的平方
+- `toFixed()`：可以把 number 四捨五入為指定小數點後的位數。
+- if 判斷 BMI 範圍，將值存入，畫面印出值。
+
+```javascript
+let bmiVal = weightVal / ((tallVal / 100) * (tallVal / 100));
+let bmiCount = bmiVal.toFixed(2);
+```
+
+## 事件偵聽
+
+- 事件偵聽：結果按鈕、更新按鈕、結果清單。
+- `e.target.dataset.num`：用 data-num 的數字順序，做為要刪除的依據。
+- `e.target.matches('i')`：判斷是否點選到 i 才執行刪除。
+- 動態產出 li 清單數量。
+
+```javascript
+for (let i = 0; i < items.length; i++) {
+    strLi += `
+      <li class="item ${items[i].color}">
+        <div class="r-circle ${items[i].color}"><span class="r-icon ${items[i].color}"></span></div>
+        <div class="r-status ${items[i].color}">${items[i].status}</div>
+        <div class="r-BMI"><span>BMI</span>${items[i].bmi}</div>
+        <div class="r-weight"><span>weight</span>${items[i].weight}kg</div>
+        <div class="r-height"><span>height</span>${items[i].tall}cm</div>
+        <div class="r-date">${today}</div>
+        <i class="fas fa-trash-alt" data-num="${i}"></i>
+      </li>
+    `;
+  }
+  resultList.innerHTML = strLi;
+```
+
+## 初始化頁面
+
+- `updateList(data)`：初始化更新頁面，有就帶 data getItem 的值，沒有就空陣列
+
+```javascript
+let data = JSON.parse(localStorage.getItem('bmiData')) || []; 
+```
---
+++ js/scripts.js
@@ -0,0 +1,176 @@
+let resultBtn = document.querySelector('.result-btn');
+let resetBtn = document.querySelector('.reset-btn');
+let resetIcon = document.querySelector('.reset');
+let resultList = document.querySelector('.record-list');
+let tallTxt = document.querySelector('.input-tall');
+let weightTxt = document.querySelector('.input-weight');
+let data = JSON.parse(localStorage.getItem('bmiData')) || [];
+
+/*----------  監聽與更新  ----------*/
+resultBtn.addEventListener('click', resultData);
+resetIcon.addEventListener('click', resetData);
+resultList.addEventListener('click', removeData);
+updateList(data);
+
+/*----------  加入結果資料  ----------*/
+function resultData(e) {
+  let tallVal = tallTxt.value;
+  let weightVal = weightTxt.value;
+  let statusTxt = document.querySelector('.r-status');
+  let bmiTxt = document.querySelector('.bmi');
+  let pTxt = document.querySelector('.status');
+  let spanTxt = document.querySelector('.r-txt span');
+  let iconBg = document.querySelector('.icon');
+  let statusColor = '';
+
+  let bmiVal = weightVal / ((tallVal / 100) * (tallVal / 100));
+  let bmiCount = bmiVal.toFixed(2);
+
+  if (18.5 <= bmiCount && bmiCount <= 25) {
+    statusTxt = '理想';
+    pTxt.textContent = '理想';
+    bmiTxt.textContent = bmiCount;
+    pTxt.classList.add('healthy');
+    bmiTxt.classList.add('healthy');
+    spanTxt.classList.add('healthy');
+    resetBtn.classList.add('healthy');
+    iconBg.classList.add('healthy');
+    statusColor = 'healthy';
+  } else if (18.5 >= bmiCount) {
+    statusTxt = '過輕';
+    pTxt.textContent = '過輕';
+    bmiTxt.textContent = bmiCount;
+    pTxt.classList.add('underweight');
+    bmiTxt.classList.add('underweight');
+    spanTxt.classList.add('underweight');
+    resetBtn.classList.add('underweight');
+    iconBg.classList.add('underweight');
+    statusColor = 'underweight';
+  } else if (25 <= bmiCount && bmiCount <= 30) {
+    statusTxt = '過重';
+    pTxt.textContent = '過重';
+    bmiTxt.textContent = bmiCount;
+    pTxt.classList.add('overweight');
+    bmiTxt.classList.add('overweight');
+    spanTxt.classList.add('overweight');
+    resetBtn.classList.add('overweight');
+    iconBg.classList.add('overweight');
+    statusColor = 'overweight';
+  } else if (30 <= bmiCount && bmiCount <= 35) {
+    statusTxt = '輕度肥胖';
+    pTxt.textContent = '輕度肥胖';
+    bmiTxt.textContent = bmiCount;
+    pTxt.classList.add('obese-1');
+    bmiTxt.classList.add('obese-1');
+    spanTxt.classList.add('obese-1');
+    resetBtn.classList.add('obese-1');
+    iconBg.classList.add('obese-1');
+    statusColor = 'obese-1';
+  } else if (35 <= bmiCount && bmiCount <= 40) {
+    statusTxt = '中度肥胖';
+    pTxt.textContent = '中度肥胖';
+    bmiTxt.textContent = bmiCount;
+    pTxt.classList.add('obese-2');
+    bmiTxt.classList.add('obese-2');
+    spanTxt.classList.add('obese-2');
+    resetBtn.classList.add('obese-2');
+    iconBg.classList.add('obese-2');
+    statusColor = 'obese-2';
+  } else if (40 <= bmiCount) {
+    statusTxt = '重度肥胖';
+    pTxt.textContent = '重度肥胖';
+    bmiTxt.textContent = bmiCount;
+    pTxt.classList.add('obese-3');
+    bmiTxt.classList.add('obese-3');
+    spanTxt.classList.add('obese-3');
+    resetBtn.classList.add('obese-3');
+    iconBg.classList.add('obese-3');
+    statusColor = 'obese-3';
+  }
+
+  let totalResult = {
+    tall: tallVal,
+    weight: weightVal,
+    bmi: bmiCount,
+    status: statusTxt,
+    color: statusColor
+  };
+
+  let tallError = document.querySelector('.tall-error');
+  let weightError = document.querySelector('.weight-error');
+
+  if (tallVal === null || tallVal === '' || weightVal === null || weightVal === '') {
+    tallError.textContent = '* 請填寫身高';
+    tallTxt.classList.add('error');
+    weightError.textContent = '* 請填寫體重';
+    weightTxt.classList.add('error');
+    return;
+  } else if (isNaN(tallVal) || isNaN(weightVal)) {
+    tallError.textContent = '* 請填寫數字';
+    tallTxt.classList.add('error');
+    weightError.textContent = '* 請填寫數字';
+    weightTxt.classList.add('error');
+    return;
+  } else {
+    tallTxt.classList.remove('error');
+    weightTxt.classList.remove('error');
+    tallError.textContent = '';
+    weightError.textContent = '';
+  }
+
+  data.push(totalResult);
+  updateList(data);
+  changeBtn();
+  localStorage.setItem('bmiData', JSON.stringify(data));
+}
+
+/*----------  更新內容  ----------*/
+function updateList(items) {
+  let date = new Date();
+  let yyyy = date.getFullYear();
+  let mm = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+  let dd = date.getDate();
+  let today = mm + '-' + dd + '-' + yyyy;
+
+  let strLi = '';
+  for (let i = 0; i < items.length; i++) {
+    strLi += `
+      <li class="item ${items[i].color}">
+        <div class="r-circle ${items[i].color}"><span class="r-icon ${items[i].color}"></span></div>
+        <div class="r-status ${items[i].color}">${items[i].status}</div>
+        <div class="r-BMI"><span>BMI</span>${items[i].bmi}</div>
+        <div class="r-weight"><span>weight</span>${items[i].weight}kg</div>
+        <div class="r-height"><span>height</span>${items[i].tall}cm</div>
+        <div class="r-date">${today}</div>
+        <i class="fas fa-trash-alt" data-num="${i}"></i>
+      </li>
+    `;
+  }
+  resultList.innerHTML = strLi;
+}
+
+/*----------  更新按鈕  ----------*/
+function changeBtn(items) {
+  resultBtn.style.display = 'none';
+  resetBtn.style.display = 'block';
+}
+
+/*----------  清空，重設輸入內容  ----------*/
+function resetData() {
+  resultBtn.style.display = 'block';
+  resetBtn.style.display = 'none';
+  tallTxt.value = '';
+  weightTxt.value = '';
+}
+
+/*----------  刪除內容  ----------*/
+function removeData(e) {
+  e.preventDefault();
+  let num = e.target.dataset.num;
+  if (e.target.matches('i')) {
+    data.splice(num, 1);
+  }
+
+  localStorage.setItem('bmiData', JSON.stringify(data));
+  updateList(data);
+}
